0fd3da875e84cc8c9e2df472e9beb2fede766fc0
Ivo Saavedra <46604808+ivSaav@users.noreply.github.com>
2020-11-15 21:39:03 +0000

    finished generate valid ring moves

--- validation.pl
+++ validation.pl
@@ -1,24 +1,49 @@
 calculateValidPlays(Board, PlayerID, ValidPlays) :-
     getValidRingMoves(Board, PlayerID, ValidPlays).
-   
 
-getValidRingMoves(Board, PlayerID, Ret) :- 
-    getStashSize(PlayerID, Size),
-    Size > 0,
+
+getValidRingMoves(Board, PlayerID, Ret) :-
+    % getStashSize(PlayerID, Size),
+    % Size > 0,
     getPlayerColor(PlayerID, Color),        % get current player's color
     selectPiece('R', Color, Piece),        % select ring with the color of the player
-    findall(('R', Piece, Line, Col), 
-        (
-            isValidRingMove(Board, Piece, Line, Col),
-            Line < 5,
-            Col < 5
-        ),
-        Ret).
+    calcLine(Board, Piece, 0, Ret).
 
 % getValidRingMoves(_,_,_). % no more valid ring moves
 
 
-isValidRingMove(Board, Piece, Line, Col) :- 
+isValidRingMove(Board, Piece, Line, Col) :-
     getTopXY(Board, Line, Col, Top),
+    !,
     playableOn(Piece, Top). % assert if ring is playable on top of stack X Y
+
+
+calcLine(Board, Piece, Line, List) :-
+    Line < 5,
+    calcCol(Board, Piece, Line, 0, New),
+    append(_, New, List),
+    L1 is Line+1,
+    calcLine(Board, Piece, L1, List).
+
+calcLine(_,_,_,Ret).
+
+
+calcCol(Board, Piece, Line, Col, [Head|Tail]) :-
+    Col < 5,
+    evaluateCell(Board, Piece, Line, Col, Res),
+    append(_, Res, Head),
+    C1 is Col+1,
+    calcCol(Board, Piece, Line, C1, Tail).
+    
+calcCol(_,_,_,_,Ret).
+
+evaluateCell(Board, Piece, Line, Col, Ret) :-
+    isValidRingMove(Board, Piece, Line, Col),
+    append(_, ['R', Piece, Line, Col], Ret).
+
+evaluateCell(_,_,_,_,[]).
+
+
+
+
 
