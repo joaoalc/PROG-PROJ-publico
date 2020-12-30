5b27c0fa96d600a334a9aed70e3358a962c5839e
up201806531 <up201806531@fe.up.pt>
2020-12-30 00:16:48 +0000

    Added board randomization that always returns a board with at least one result

--- proj2/boardRandomization.pl
+++ proj2/boardRandomization.pl
@@ -30,3 +30,46 @@ randomizeLine([]).
 randomizeLine([First|Line]) :-
     randomBoardNumber(First),
     randomizeLine(Line).
+
+
+solveRandomSuccessfulLightbulb(RowLen, ColLen) :- %Refactor the lightbuld function to use flattened boards; use setof here to find every solution then use statistics and such
+    randomSuccessfulLightbulb(FlattenedResults, FlattenedNumbers, RowLen, ColLen),
+    BoardSize is RowLen * ColLen,
+    length(FlatRes, BoardSize),
+    domain(FlatRes, 0, 1), %1 is lit, 0 is unlit
+    sum(FlatRes, #\=, 0), %Exclude all zeros
+    write(FlattenedResults), write(FlattenedNumbers),
+    restrictSpot(FlattenedNumbers, FlatRes, RowLen, ColLen, 1),
+    write('.'),
+   labeling([], FlatRes),
+   write(FlatRes).
+
+randomSuccessfulLightbulb(FlattenedResults, FlattenedNums, RowLen, ColLen) :-
+    generateVariableBoard(ResultBoard, RowLen, ColLen),
+    flatten(ResultBoard, FlattenedResults),
+    randomizeResultBoard(FlattenedResults),
+    generateVariableBoard(NumberBoard, RowLen, ColLen),
+    flatten(NumberBoard, FlattenedNumbers),
+    setof(FlattenedNumbers, restrictions(FlattenedNumbers, FlattenedResults, RowLen, ColLen), List),
+    random_member(FlattenedNums, List).
+
+
+restrictions(FlattenedNumbers, FlattenedResults, RowLen, ColLen) :-
+    domain(FlattenedNumbers, 1, 9),
+    restrictSpot(FlattenedNumbers, FlattenedResults, RowLen, ColLen, 1),
+    labeling([], FlattenedNumbers).
+
+randomizeResultBoard([]).
+
+randomizeResultBoard([First|FlattenedResults]) :-
+    random(0, 2, First),
+    randomizeResultBoard(FlattenedResults).
+
+generateVariableBoard(ResultBoard, RowLen, Collen) :-
+   length(ResultBoard, Collen),
+   createMatrix(ResultBoard, RowLen).
+
+   
+
+
+
